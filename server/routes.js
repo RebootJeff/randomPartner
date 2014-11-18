184ecde053b363206f795ccfee638c7590882954
Jeff Lee <leejeffc@gmail.com>
2014-11-17 19:11:04 -0800

    Move server router code to its own file

---
+++ server/routes.js
@@ -0,0 +1,24 @@
+// Internal Dependencies
+var classroomsCtrl = require('./classroomsController');
+
+function setupRoutes(app) {
+  app.route('/api/dev')
+    .get(function(req, res, next){
+      // I'm an example! Hell yes.
+    })
+    .post(function(req, res, next){
+      // I'm also an example! For future reference, no doubt.
+    });
+
+  app.route('/api/classroom/create').post(classroomsCtrl.create);
+
+  // Default to home page
+  // TODO: Fix this to serve 404 page as appropriate
+  app.get('/*', function(req, res, next) {
+    res.sendFile('index.html', {
+      root: __dirname + '/../dist'
+    });
+  });
+}
+
+module.exports = setupRoutes;
--- server/server.js
+++ server/server.js
@@ -30,33 +30,17 @@ if(env === 'development') {
 }
 database.connect(dbUrl).then(function() {
   console.log('Connected to database.');
+  return database.insert('testCollection', { testKey: 'hiiii!' });
 }).catch(function(reason) {
   console.error('Error trying to connect to database:', reason);
 });
 
 
-// ============================================================================
-// Basic routes
-// ============================================================================
-app.route('/api/dev')
-  .get(function(req, res, next){
-
-  })
-  .post(function(req, res, next){
-
-  });
-
-// Default to home page
-// TODO: Fix this to serve 404 page as appropriate
-app.get('/*', function(req, res, next) {
-  res.sendFile('index.html', {
-    root: __dirname + '/../dist'
-  });
-});
+// Setup routing
+var setupRoutes = require('./routes');
+setupRoutes(app);
 
 
-// ============================================================================
 // Start server
-// ============================================================================
 app.listen(8000);
 console.log('Listening on port 8000...');
