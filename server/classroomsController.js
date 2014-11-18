7912bac5ace923bb5739334c4b14c02997b69994
Jeff Lee <leejeffc@gmail.com>
2014-11-17 19:44:03 -0800

    Add server controller for making new classrooms

---
+++ server/classroomsController.js
@@ -0,0 +1,38 @@
+// Internal Dependencies
+var database = require('./promisifiedDb');
+
+var classroomsCtrl = {};
+
+classroomsCtrl.findByHash = function(req, res, next) {
+
+  database.find({
+    hash: req.params.hash
+  }).then(function(foundClassroom) {
+    res.send(200, foundClassroom);
+
+  }).catch(function(err) {
+    res.send(404, err);
+  });
+
+};
+
+classroomsCtrl.create = function(req, res, next) {
+
+  database.insert('classrooms', {
+    // hash: TODO - generate SHA-1 hash here,
+    name: req.body.name,
+    students: [],
+    teacher: req.body.teacher
+  }).then(function(createdClassroom) {
+    res.send(201, createdClassroom);
+
+  }).catch(function(err) {
+    res.send(500, err);
+  });
+
+};
+
+
+
+module.exports = classroomsCtrl;
+
